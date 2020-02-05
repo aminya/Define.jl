8780afd4118c7780799568ec27dd0f7580991005
aminya <aminyahyaabadi74@gmail.com>
2020-02-05 03:13:54 -0600

    Tokenize utilities - UndefToken

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,1 +1,8 @@
 Meta.parse(t::T) where {T <: Union{Token, Array{Token}}} = Meta.parse(untokenize(t))
+
+"""
+    UndefToken
+
+An abstract type that shows if the an evaluated `Token` is defined.
+"""
+abstract type UndefToken end
