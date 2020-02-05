7f679c2cdd9463f62f4c65b4bf31cfe0fdd30d5d
aminya <aminyahyaabadi74@gmail.com>
2020-02-05 03:14:43 -0600

    Tokenize utilities - tevalfast

--- src/utilities.jl
+++ src/utilities.jl
@@ -32,3 +32,29 @@ tisdefined(modul, t::T) where {T <: Union{Symbol, Expr}} = isdefined(modul, t)
 tisdefined(t::T) where {T <: Union{Symbol, Expr}} = isdefined(@__MODULE__, t)
 
 
+"""
+    tevalfast(t::T)
+    tevalfast(t::T, check_isdefined::Bool=false)
+    tevalfast(modul = @__MODULE__, t::T, check_isdefined::Bool=false)
+
+Parses and evaluates a `Token` that represents a `Symbol` or `Expr` in Julia. For `Symbol` It dispatches on the fast method based on the parsed Token.
+
+If you set `check_isdefined` to `true`, and `t` is not defined in the scope it returns `UndefToken` instead of throwing an error.
+
+# Examples
+```julia
+julia> t = collect(tokenize("Int64"))
+
+julia> tevalfast(t)
+Int64
+```
+"""
+function tevalfast(modul::Module, t::T, check_isdefined::Bool = false) where {T <: Union{Token, Array{Token}}}
+    pt = Meta.parse(t)
+    if check_isdefined && !(tisdefined(modul,pt))
+        return UndefToken
+    end
+    return evalfast(modul,pt)
+end
+
+tevalfast(t::T, check_isdefined::Bool = false) where {T <: Union{Token, Array{Token}}} = tevalfast(@__MODULE__, t, check_isdefined)
